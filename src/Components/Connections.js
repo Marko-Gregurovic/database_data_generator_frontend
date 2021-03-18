22439eee3e96d31e4fb78e56bebe6b37336d4bb8
Marko-Gregurovic <marko.gregurovic0304@gmail.com>
2021-03-18 12:47:38 +0100

    message

--- src/Components/Connections.js
+++ src/Components/Connections.js
@@ -10,7 +10,7 @@ import { API_URL } from "../helpers/Constants";
 const Connections = (props) => {
     const { auth, dispatch } = useAuth();
     const history = useHistory();
-    const [connections, setConnections] = useState([]);
+    const [connections, setConnections] = useState(null);
 
 
     let RequestConnections = {
@@ -46,7 +46,7 @@ const Connections = (props) => {
     }
 
     useEffect(() => {
-        dispatch({type: RESET_ERROR});
+        dispatch({ type: RESET_ERROR });
         let error = false;
         const REST_API_URL = API_URL + "/user/connections";
         fetch(REST_API_URL, {
@@ -75,44 +75,59 @@ const Connections = (props) => {
     }, []);
 
     return (
-        <Container fluid className=" flex-grow-1 align-content-center card-columns">
-            <Link to="/user/connections/add">
-                <div className="card text-center rounded-lg shadow-sm bg-myblue text-mylightblack">
-                    <div className="card-header">
-                        <h5 className="card-title">Add Connection</h5>
-                    </div>
-                    <div className="card-body bg-mylightblack text-myblue" style={{ fontSize: "8.25em" }}>
-                        <p className="card-text">+</p>
-                    </div>
+        <>
+            {connections == null ?
+                <div
+                    className="container 
+                   d-flex
+                   align-items-center
+                   justify-content-center
+                   text-myblue
+                   display-1
+                   main-text">
+                    Loading...
                 </div>
-            </Link>
+                :
 
-            {
-                connections.map(connection => {
-                    return (
-                        <div className="card text-center rounded-lg shadow-sm bg-myblue text-mylightblack" key={connection.connectionId}>
+                <Container fluid className=" flex-grow-1 align-content-center card-columns">
+                    <Link to="/user/connections/add">
+                        <div className="card text-center rounded-lg shadow-sm bg-myblue text-mylightblack">
                             <div className="card-header">
-                                <h5 className="card-title">{connection.host}: {connection.database}</h5>
+                                <h5 className="card-title">Add Connection</h5>
                             </div>
-                            <div className="card-body bg-mylightblack text-myblue">
-                                <p className="card-text">Host: {connection.host}</p>
-                                <p className="card-text">Database: {connection.database}</p>
-                                <p className="card-text">User: {connection.username}</p>
-                                <p className="card-text">SQL platform: {connection.sqlPlatform}</p>
-                                <ButtonGroup>
-
-                                    <Button className="bg-danger" onClick={(event) =>
-                                        DeleteConnection(connection.connectionId)}
-                                    >Delete</Button>
-                                    <Button className="bg-myblue text-mylightblack">Connect</Button>
-                                </ButtonGroup>
+                            <div className="card-body bg-mylightblack text-myblue" style={{ fontSize: "8.25em" }}>
+                                <p className="card-text">+</p>
                             </div>
                         </div>
+                    </Link>
 
-                    );
-                })
-            }
-        </Container>
+                    {
+                        connections.map(connection => {
+                            return (
+                                <div className="card text-center rounded-lg shadow-sm bg-myblue text-mylightblack" key={connection.connectionId}>
+                                    <div className="card-header">
+                                        <h5 className="card-title">{connection.host}: {connection.database}</h5>
+                                    </div>
+                                    <div className="card-body bg-mylightblack text-myblue">
+                                        <p className="card-text">Host: {connection.host}</p>
+                                        <p className="card-text">Database: {connection.database}</p>
+                                        <p className="card-text">User: {connection.username}</p>
+                                        <p className="card-text">SQL platform: {connection.sqlPlatform}</p>
+                                        <ButtonGroup>
+
+                                            <Button className="bg-danger" onClick={(event) =>
+                                                DeleteConnection(connection.connectionId)}
+                                            >Delete</Button>
+                                            <Button className="bg-myblue text-mylightblack">Connect</Button>
+                                        </ButtonGroup>
+                                    </div>
+                                </div>
+
+                            );
+                        })
+                    }
+                </Container>}
+        </>
     );
 }
 
